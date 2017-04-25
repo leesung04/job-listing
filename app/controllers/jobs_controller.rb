e067cbe9106e311822efe41f14bcd85d158cad14
齐天大圣 <qitiandasheng@qitiandashengdeiMac.local>
2017-04-25 16:43:27 +0800

    use before_action to find_group_and_check_permission

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,5 +1,6 @@
 class JobsController < ApplicationController
   before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
+  before_action :find_job_and_check_permission, only: [:edit, :update, :destroy]
 
   def index
     @jobs = Job.all
@@ -28,18 +29,10 @@ class JobsController < ApplicationController
 
 
   def edit
-   @job = Job.find(params[:id])
-   if current_user != @job.user
-     redirect_to root_path, alert: "对不起， 您没有访问权限！"
-   end
   end
 
 
   def update
-   @job = Job.find(params[:id])
-   if current_user != @job.user
-     redirect_to root_path, alert: "对不起， 您没有访问权限！"
-   end
    if @job.update(job_params)
    redirect_to jobs_path, notice: "修改成功"
    else
@@ -49,17 +42,20 @@ class JobsController < ApplicationController
 
 
   def destroy
-    @job = Job.find(params[:id])
-    if current_user != @job.user
-      redirect_to root_path, alert: "对不起， 您没有访问权限！"
-    end
     @job.destroy
-    flash[:alert] = "职位已删除"
-    redirect_to jobs_path
+    redirect_to jobs_path, alert: "职位已删除"
   end
 
 
   private
+  def find_job_and_check_permission
+    @job = Job.find(params[:id])
+
+    if current_user != @job.user
+      redirect_to root_path, alert: “对不起，您没有访问权限！”
+    end
+  end
+
   def job_params
     params.require(:job).permit(:title, :description, :max, :min, :email)
   end
