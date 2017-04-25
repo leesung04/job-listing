ddbaf2d17907f534c6c9c28a27895a344726f33c
齐天大圣 <qitiandasheng@qitiandashengdeiMac.local>
2017-04-25 13:46:39 +0800

    create groups index

---
+++ db/migrate/20170425053830_create_jobs.rb
@@ -0,0 +1,13 @@
+class CreateJobs < ActiveRecord::Migration[5.0]
+  def change
+    create_table :jobs do |t|
+      t.string :title
+      t.text :description
+      t.text :max
+      t.text :min
+      t.text :email
+
+      t.timestamps
+    end
+  end
+end
